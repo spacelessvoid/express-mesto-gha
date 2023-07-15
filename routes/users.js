d5976e58c2204334975995326d57b2ba458518ed
Aleksey Tishenkov <tishenkov@gmail.com>
2023-07-16 00:10:28 +0300

    fix: correct validation of id-s in routes (should be hex instead of alphanum)

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.get("/me", celebrate({
 router.get("/", getUsers);
 router.get("/:userId", celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
+    userId: Joi.string().length(24).hex().required(),
   }),
 }), getUserById);
 router.patch("/me", celebrate({
